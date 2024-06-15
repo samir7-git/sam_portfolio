42cbb8a94c6c62ba0a47cba9538003ceb0ba2a41
samir7-git <samirmujawar000@gmail.com>
2024-06-15 06:08:26 +0000

    change image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "/workspaces/sam_portfolio/__images__/samantha-gades-BlIhVfXbi9s-unsplash.jpg";
+import image from "../images/samantha.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
